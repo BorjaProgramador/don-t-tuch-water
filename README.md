d2dbed0c4df1f27eceefe296310d21f7e7971605
BorjaProgramador <138173064+BorjaProgramador@users.noreply.github.com>
2023-06-29 20:42:13 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Abra esta pagina en [https://borjaprogramador.github.io/don-t-tuch-water/](https://borjaprogramador.github.io/don-t-tuch-water/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/borjaprogramador/don-t-tuch-water** e importar
+
+## Edita este proyecto ![Build status badge](https://github.com/borjaprogramador/don-t-tuch-water/workflows/MakeCode/badge.svg)
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/borjaprogramador/don-t-tuch-water** y haga clic en importar
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/borjaprogramador/don-t-tuch-water/raw/master/.github/makecode/blocks.png)
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,61 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . 
+e e e . . . . e e e . . . . 
+c d d c . . c d d c . . . . 
+c b d d f f d d b c . . . . 
+c 3 b d d b d b 3 c . . . . 
+f b 3 d d d d 3 b f . . . . 
+e d d d d d d d d e . . . . 
+e d f d d d d f d e b f b . 
+f d d f d d f d d f f d f . 
+f b d d b b d d 2 b f d f . 
+. f 2 2 2 2 2 2 d b d b f . 
+. f d d d d d d d f f f . . 
+. f d b d f f f d f . . . . 
+. . f f f f . . f f . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . e b b e e e b b b b b e e 
+. e e b b b b e b b b b b b b e 
+. e b b b b b b b b b b b b b b 
+e b b b b b b b b b b b b b b b 
+e b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b e e e b b b b b b b b 
+e e b e e e e b b b e e b b b e 
+b b e e b b e b b b e e e e e b 
+b e e e b b e e e f f b b e b b 
+f e b b e f f e f e e f e e e f 
+f f e e e e e f f e e f f f f f 
+f f f f e e f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
